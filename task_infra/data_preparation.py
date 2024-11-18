aef3287d1d731bf805155246ef03a826bbcb3df3
yuvalorsher <u.vorsher@gmail.com>
2024-11-18 12:02:20 +0200

    No real changed

--- task_infra/data_preparation.py
+++ task_infra/data_preparation.py
@@ -13,10 +13,11 @@ class DataPrep(Task):
         data_loader = DataLoader.get_loader(self.params['data_loader_params'])
         self.subtasks.append(('DataLoader', data_loader))
         value_clipper = ValueClipper(self.params['clipper_params'], data_loader.outputs['raw_data'])
-        self.subtasks.append('ValueClipper', value_clipper)
+        self.subtasks.append(('ValueClipper', value_clipper))
 
     def get_prediction_steps(self):
         return self.get_sub_tasks_predicion_steps()
+
 
 class ValueClipper(Task):
     clipped_suffix = '_clipped'
