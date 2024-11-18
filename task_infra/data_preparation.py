c92f6d8cace11b77fc729fd77e085b7f25e29169
yuvalorsher <u.vorsher@gmail.com>
2024-11-18 13:16:48 +0200

    Use class var to name output df

--- task_infra/data_preparation.py
+++ task_infra/data_preparation.py
@@ -8,12 +8,14 @@ from sklearn.pipeline import Pipeline
 
 
 class DataPrep(Task):
+    output_df_key = 'clean_df'
 
     def run(self):
         data_loader = DataLoader.get_loader(self.params['data_loader_params'])
         self.subtasks.append(('DataLoader', data_loader))
-        value_clipper = ValueClipper(self.params['clipper_params'], data_loader.outputs['raw_data'])
+        value_clipper = ValueClipper(self.params['clipper_params'], data_loader.outputs[data_loader.output_df_key])
         self.subtasks.append(('ValueClipper', value_clipper))
+        self.outputs[self.output_df_key] = value_clipper[value_clipper.output_df_key]
 
     def get_prediction_steps(self):
         return self.get_sub_tasks_predicion_steps()
@@ -21,6 +23,7 @@ class DataPrep(Task):
 
 class ValueClipper(Task):
     clipped_suffix = '_clipped'
+    output_df_key = 'clipped_df'
 
     def transform(self, df: pd.DataFrame):
         df_transformed = df.assign(
@@ -31,24 +34,25 @@ class ValueClipper(Task):
         )
         return df_transformed
 
-    def fit(self, X, y):
+    def fit(self, x, y):
         return self
 
     def run(self):
-        self.outputs['clipped_df'] = self.transform(self.input_df)
+        self.outputs[self.output_df_key] = self.transform(self.input_df)
 
     def get_prediction_steps(self):
         return Pipeline(steps=['ValueClipper', self])
 
 
-
 class DataLoader(Task):
+    output_df_key = 'raw_data'
+
     @abstractmethod
     def load_data(self) -> pd.DataFrame:
         raise NotImplementedError()
 
     @staticmethod
-    def get_loader(data_loader_params: dict) -> Type[DataLoader]:
+    def get_loader(data_loader_params: dict) -> DataLoader:
         data_loader = {
             CsvDataLoader.data_type: CsvDataLoader(data_loader_params),
         }.get(data_loader_params['data_type'], None)
@@ -69,7 +73,7 @@ class CsvDataLoader(DataLoader):
         super().__init__(params)
 
     def run(self) -> None:
-        self.outputs['raw_data'] = self.load_data()
+        self.outputs[self.output_df_key] = self.load_data()
 
     def load_data(self) -> pd.DataFrame:
         return pd.read_csv(self.data_path, **self.additional_load_params)
