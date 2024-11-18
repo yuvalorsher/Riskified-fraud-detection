3dac5752584cc5deed749688c10b43c208f24901
yuvalorsher <u.vorsher@gmail.com>
2024-11-18 09:07:17 +0200

    Delete ExperimentParams class

--- task_infra/experiment_pipeline.py
+++ task_infra/experiment_pipeline.py
@@ -6,27 +6,6 @@ from task_infra.task import Task
 from task_infra.data_preparation import DataPrep
 
 
-# class ExperimentParams:
-#     def __init__(self, path_to_parameters_file: str):
-#         self.path_to_parameters_file = path_to_parameters_file
-#         self.params_dict: dict | None = None
-#
-#     def load_parameters(self, override: bool = False):
-#         if self.params_dict is not None and override is True:
-#             raise ValueError("Parameters already loaded. To overide, call with 'override=True'")
-#         self.params_dict = self.load_params_from_file(path_to_parameters_file)
-#
-#     @staticmethod
-#     def load_params_from_json_txt(json_txt: str):
-#         return json.loads(json_txt)
-#
-#     @staticmethod
-#     def load_params_from_file(params_file_path: str) -> dict:
-#         with open(params_file_path) as f:
-#             parameters_dict = json.load(f)
-#         return parameters_dict
-
-
 class Experiment(Task):
     def __init__(self,
                  data_prep_params: dict,
