df2953eedcaecc5769be5bdbe65c9e275463b970
yuvalorsher <u.vorsher@gmail.com>
2024-11-18 15:56:48 +0200

    Append train_test_split to TrainModel subtasks

--- task_infra/train.py
+++ task_infra/train.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
 from abc import ABC, abstractmethod
-from task_infra.task import Task
 from typing import Type
 from sklearn.pipeline import Pipeline
 
+from task_infra.task import Task
+from task_infra.sampling import Sampler
 from task_infra.consts import DATE_COL
+
 
 class TrainModel(Task):
 
     def run(self):
         train_test_split = TrainTestSplitter(self.params['train_test_split_params'], self.input_df)
+        self.subtasks.append(('TrainTestSplit', train_test_split))
+
 
     def get_prediction_steps(self):
         return self.get_sub_tasks_predicion_steps()
+
 
 class TrainTestSplitter(Task):
     """
@@ -28,7 +33,7 @@ class TrainTestSplitter(Task):
             (self.input_df[DATE_COL] >= self.params['train_start_date']) &
             (self.input_df[DATE_COL] < self.params['test_start_date'])
         ]
-        self.outputs[selftest_df_key] = self.input_df[
+        self.outputs[self.test_df_key] = self.input_df[
             (self.input_df[DATE_COL] >= self.params['test_start_date']) &
             (self.input_df[DATE_COL] < self.params['test_end_date'])
             ]
@@ -40,6 +45,7 @@ class TrainTestSplitter(Task):
     def run_asserts(self) -> None:
         """
         Ideally this gets a list of which assert objects to run, indicated in params json, but I had no time to implement.
+        Currently asserts dfs are not empty, can be configured to allow this.
         """
         assert len(self.outputs['train_df']) > 0, f"Training data df is empty! When splitting with params {self.params}"
         assert len(self.outputs['test_df']) > 0, f"Test data df is empty! When splitting with params {self.params}"
