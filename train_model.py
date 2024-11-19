95e702efe120eefa0abef121424bcaf46bc32421
yuvalorsher <u.vorsher@gmail.com>
2024-11-19 18:55:32 +0200

    Allow saving or loading full experiment

--- train_model.py
+++ train_model.py
@@ -1,16 +1,25 @@
 import json
 from task_infra.experiment_pipeline import Experiment
 from task_infra.reporter import Reporter
+import pickle
 
 json_file = "experiment_params.json"
-report_output_path = "experiment_report.html"
-save_trained_model_to = "trained_model.pkl"
+report_savepath = "experiment_report.html"
+trained_model_savepath = "trained_model.pkl"
+
+load_experiment = False
+load_experiment_path = "yuval_full_experiment.pkl"  # Will only be used if load_experiment is True
+full_experiment_savepath = "full_experiment.pkl"
 
 if __name__ == '__main__':
-    with open(json_file) as f:
-        params = json.load(f)
-
-    exp = Experiment(params)
-    exp.save_trained_model(save_trained_model_to)
+    if load_experiment:
+        with open(full_experiment_savepath, 'rb') as handle:
+            exp = pickle.load(handle)
+    else:
+        with open(json_file) as f:
+            params = json.load(f)
+        exp = Experiment(params)
+        exp.save_experiment(full_experiment_savepath)
+        exp.save_trained_model(trained_model_savepath)
     reporter = Reporter.create_report_from_experiment(exp)
-    reporter.report(report_output_path)
+    reporter.report(report_savepath)
