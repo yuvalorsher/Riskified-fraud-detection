67d27a233ecdc9721156b6af55abb82bcb809bf1
yuvalorsher <u.vorsher@gmail.com>
2024-11-19 18:21:01 +0200

    Add reported and change paths to avoid override when they run

--- predict.py
+++ predict.py
@@ -3,13 +3,13 @@ import pandas as pd
 import pickle
 
 path_to_data = './Data/transactions.csv'
-path_to_model = 'trained_model.pkl'
+path_to_model = 'yuval_trained_model.pkl'
 path_to_prediction_csv = 'predictions.csv'
-predict_samples = 100 # To make quicker, set to None if you want to predict on all transactions
+predict_samples = None  # To make quicker, set to None if you want to predict on all transactions
 
 
 def predictions_to_csv(predictions: np.ndarray, pred_index: pd.Index, save_path: str) -> None:
-    pd.Series(data=predictions, index=pred_index).to_csv(save_path)
+    pd.Series(data=predictions, index=pred_index, name='preds').to_csv(save_path)
 
 
 if __name__ == '__main__':
@@ -20,5 +20,5 @@ if __name__ == '__main__':
     if predict_samples is not None:
         transactions = transactions.sample(predict_samples, random_state=42)
 
-    predictions = trained_model.predict(transactions)
-    predictions_to_csv(predictions, transactions.index, path_to_prediction_csv)
+    preds = trained_model.predict(transactions)
+    predictions_to_csv(preds, transactions.index, path_to_prediction_csv)
--- train_model.py
+++ train_model.py
@@ -1,7 +1,9 @@
 import json
 from task_infra.experiment_pipeline import Experiment
+from task_infra.reporter import Reporter
 
 json_file = "experiment_params.json"
+report_output_path = "experiment_report.html"
 save_trained_model_to = "trained_model.pkl"
 
 if __name__ == '__main__':
@@ -10,3 +12,5 @@ if __name__ == '__main__':
 
     exp = Experiment(params)
     exp.save_trained_model(save_trained_model_to)
+    reporter = Reporter.create_report_from_experiment(exp)
+    reporter.report(report_output_path)
